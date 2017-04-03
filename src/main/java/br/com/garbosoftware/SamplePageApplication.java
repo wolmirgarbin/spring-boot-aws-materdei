57c1c27a92aa8aed12e65e9489168a9006850f98
wolmirgarbin@gmail.com <159357wolm>
2017-04-03 20:45:22 -0300

    Sample page

--- src/main/java/br/com/garbosoftware/SamplePageApplication.java
+++ src/main/java/br/com/garbosoftware/SamplePageApplication.java
@@ -2,11 +2,25 @@ package br.com.garbosoftware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @SpringBootApplication
+@Controller
 public class SamplePageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SamplePageApplication.class, args);
 	}
+
+
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("hora", new Date());
+		return "index";
+	}
+
 }
