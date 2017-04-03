17caeaa5de83f4858bb63472beaeb5221adcaf86
cesar garbin <cesargarbin@cesars-MacBook-Air.local>
2017-04-03 20:28:27 -0300

    teset

---
+++ src/main/java/br/com/garbosoftware/SamplePageApplication.java
@@ -0,0 +1,12 @@
+package br.com.garbosoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplePageApplication.class, args);
+	}
+}
